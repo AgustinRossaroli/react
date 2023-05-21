db99b6d6ac1c72f50a7396258326ecb556774d46
AgustinRossaroli <agustinrossaroli@gmail.com>
2023-05-21 16:38:39 -0300

    estilizando

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,13 @@ import Users from './components/Users'
 import Category from './components/Category'
 import LastProduct from './components/LastProduct'
 import ProductList from './components/ProductList'
+import Header from './components/Header'
+
 
 function App() {
   return (
     <div>
+      <Header />
       <Products />
       <Users />
       <Category />
---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import "./header.css";
+
+class Header extends Component {
+    render() {
+        return (
+
+            <header>
+                <div className="logo-box">
+                    
+                        <div className="logo">
+                            NailStore DashBoard
+                        </div>
+                    
+                </div>
+            </header>
+        );
+      }
+}
+
+export default Header;
